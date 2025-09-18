3c59fb7d26173df781030643a4e7aec37977b4e6
ScamStoryteller <scamstoryteller@gmail.com>
2025-09-18 17:31:36 -0400

    added a few more 8ball responses

--- src/main/java/nailheadbot/magic8Ball.java
+++ src/main/java/nailheadbot/magic8Ball.java
@@ -3,23 +3,28 @@ package nailheadbot;
 import java.util.Random;
 
 public class magic8Ball {
+    public static final Random rand = new Random();
     public static String[] responses = {
             "yuh-huh",
             "correctamundo!",
             "so true bestie!!!",
             "survey says...*EXTREMELY LOUD CORRECT DING*",
             "YES! YES! YES!",
+            "hell to the yeah",
+            "idk probably",
+            "yeah that seems about right",
             "nuh-uh",
             "nah not happening",
             "so false worstie!!!",
             "survey says...*EXTREMELY LOUD INCORRECT BUZZER*",
             "nope.",
+            "seems sketchy to me chief",
             "who tf knows bruh",
+            "huh? whuh huh?",
+            "what do you think wise guy?",
             "ask me that shit again some other time",
             "i got no clue but did you know that the mitochondria is the powerhouse of the cell"
     };
-
-    public static final Random rand = new Random();
 
     public static String run8ball() {
         int index = rand.nextInt(responses.length);
